b0dc6effdbd2890091787026d609d6cebcdaa6b6
wilmerMendoza <scaredt33@gmail.com>
2024-04-30 10:43:38 -0500

    Actualizacion de productos
    Agregamos productos a la pagina

--- P_mujer.php
+++ P_mujer.php
@@ -307,46 +307,24 @@
                         <div class="row">
                             <div class="col-12">
                                 <div class="page-title-box d-flex align-items-center justify-content-between">
-                                    <h4 class="mb-0 font-size-18">Ofertas</h4>
-
-                                    <div class="page-title-right">
-                                        <ol class="breadcrumb m-0">
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Paginas</a></li>
-                                            <li class="breadcrumb-item active">Ofertas</li>
-                                        </ol>
+                                    <div class="container-fluid row">
+                                        <?php for ($i=0; $i < 8; $i++){?>
+                                        <div class="col-md-3">
+                                            <div class="card">
+                                                <img class="card-img-top img-fluid" src="img/ropa mujer 1.webp">
+                                                <div class="card-body">
+                                                    <h5 class="card-tible">Titulo</h5>
+                                                    <p class="card-text">Descripcion</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <?php } ?>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <!-- end page title -->
-
                     </div> <!-- container-fluid -->
-                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                        <ol class="carousel-indicators">
-                          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                        </ol>
-                        <div class="carousel-inner">
-                          <div class="carousel-item active">
-                            <img class="d-block w-100" src="img/descuentos-de-ropa.webp" alt="First slide">
-                          </div>
-                          <div class="carousel-item">
-                            <img class="d-block w-100" src="img/Dw4pTJPWsAIPlQg.jpeg" alt="Second slide">
-                          </div>
-                          <div class="carousel-item">
-                            <img class="d-block w-100" src="img/descarga.webp" alt="Third slide">
-                          </div>
-                        </div>
-                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                          <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                          <span class="sr-only">Next</span>
-                        </a>
-                      </div>
                 </div>
                 <!-- End Page-content -->
 
